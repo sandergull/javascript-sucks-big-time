ed3257c9165bb2c556b4cf40c620ab74e44a2d9f
Sander Gulliksen <sander.gulliksen@soprasteria.com>
2025-10-23 16:21:36 +0200

    taskmode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './styles/layout.css';
 
-const MODE = 'solution' as 'solution' | 'task';
+const MODE = 'task' as 'solution' | 'task';
 
 import SolutionAccordionPage from './solution/pages/AccordionPage';
 import SolutionTabsPage from './solution/pages/TabsPage';
